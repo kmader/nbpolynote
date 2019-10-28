74c26e9787a3ebdb918c7f13cdad69db44727256
Kevin Mader <kmader@magicleap.com>
2019-10-28 13:55:07 +0100

    updating postbuild

--- postBuild.sh
+++ postBuild.sh
@@ -1,6 +1,6 @@
 jupyter serverextension enable --py jupyter_server_proxy
 wget https://github.com/polynote/polynote/releases/download/0.2.8/polynote-dist.tar.gz
-cd nbpolynote; tar -xvf polynote-dist.tar.gz; cd ..
+cd nbpolynote; tar -xvf ../polynote-dist.tar.gz; cd ..
 python setup.py develop
 jupyter serverextension enable  --user --py nbpolynote
 jupyter nbextension     install --user --py nbpolynote
